cf7b00958010ed81d8876d498b166736b170426a
eduardo <100406015@alumnos.uc3m.es>
2020-05-25 12:44:42 +0200

    Initial commit

---
+++ src/model.js
@@ -0,0 +1,165 @@
+class ModelError extends Error {
+    constructor(...args) {
+        super(...args);
+        this.name = 'Model Error';
+    }
+}
+
+var defaultErrors = {
+    required: (path) => `Required property "${path}" missing.`,
+    type: (path, type) => `Type of property "${path}" should be ${type}.`,
+    enum: (path, enumVals) => `Property "${path}" should be ${enumVals.join(', ')}.`,
+    range: (path, range, value) => {
+        if(typeof value == 'string') {
+            return `Property "${path}" should have a length between ${range.join(' and ')}.`;
+        } else if(typeof value == 'number') {
+            return `Property "${path}" should be a number between ${range.join(' and ')}.`;
+        } else if (Array.isArray(value)) {
+            return `Property "${path}" should have between ${range.join(' and ')} elements.`;
+        }
+    },
+    match: (path, regex) => `Property "${path}" should match the following regex ${regex}.`
+};
+
+var api = {
+    Model: modelBuilder,
+    ModelError
+};
+
+module.exports = api;
+
+//****************************************************************************
+
+/*
+* @param schema: object | array     Rules or object of props with rules, respectively
+*/
+function modelBuilder(schema) {
+    this.validate = function validate(json) {
+        var {result, errors} = validation(schema, json);
+        throwErrors(errors);
+        return result;
+    };
+    this.schema = schema;
+}
+
+/*
+* @param rules: object              Rules to follow
+* @param value: primitive           Who needs to follow rules
+*/
+function checkValue(rules, value, nest) {
+    var errors = [];
+    for(let [rule, ruleValue] of Object.entries(rules)) {
+        if(!checkRule(rule, ruleValue, value)) {
+            errors.push(getErrorMessage(rule, rules.message, nest, ruleValue, value));
+        }
+    }
+    return errors;
+}
+
+/*
+* @param schema: object | array                 Rules or object of props with rules, respectively
+* @param data: primitive | object | array       Who needs to follow rules
+* @param nest: array                            Levels of nesting in recursive tree
+* @return result: object | array                The final object or array
+* @return errors: array                         All errors that arise in order
+*/
+function validation(schema, data) {
+    var errors = [];
+    function doValidation(schema, data, nest = [], result = {}) {
+        if(Array.isArray(schema)) {
+            errors.push(...checkValue({type: 'object'}, data, nest));
+            var object = schema[0];
+            for(let [key, rules] of Object.entries(object)) {
+                var res = doValidation(rules, data[key], [...nest, key]);
+                if(res !== undefined) {
+                    result[key] = res;
+                }
+            }
+        } else if(typeof schema == 'object') {
+            errors.push(...checkValue(schema, data, nest));
+            if(schema.type === 'array') {
+                let res = doValidation(buildArrayRules(data, schema), data, nest, []);
+                return res;
+            } else if(schema instanceof api.Model) {
+                let res = doValidation(schema.schema, data, nest);
+                return res;
+            } else {
+                return data;
+            }
+        } else {
+            console.log('invalid Schema');
+        }
+        return result;
+    }
+    return {result: doValidation(schema, data), errors};
+}
+
+function checkRule(rule, ruleValue, propValue) {
+    if(propValue === undefined) {
+        if(rule == 'required' && ruleValue) {
+            return false;
+        }
+        return true;
+    }
+    switch (rule) {
+        case 'type':
+            if(ruleValue == 'array') {
+                return Array.isArray(propValue);
+            }
+            return typeof propValue == ruleValue;
+        case 'enum':
+            return ruleValue.includes(propValue);
+        case 'match':
+            if(typeof propValue != 'string') return false;
+            var regex = new RegExp(ruleValue);
+            return regex.test(propValue);
+        case 'range':
+            var size;
+            if(Array.isArray(propValue)) {
+                size = propValue.length;
+            }
+            if(typeof propValue == 'number') {
+                size = propValue;
+            }
+            if(typeof propValue == 'string')  {
+                size = [...propValue].length;
+            }
+            return size && size <= ruleValue[1] && size >= ruleValue[0];
+        default:
+            return true;
+    }
+}
+
+function getErrorMessage(rule, message, nest, ruleValue, propValue) {
+    var path = nest.join('.');
+    function getMessage(message) {
+        switch (typeof message) {
+            case "string":
+                return message;
+            case "function":
+                return message(path, ruleValue, propValue);
+            case "object":
+                if(message[rule] !== undefined) {
+                    return getMessage(message[rule]);
+                }
+            /* falls through */
+            default:
+                return defaultErrors[rule](path, ruleValue, propValue);
+        }
+    }
+    return getMessage(message);
+}
+
+function buildArrayRules(data, schema) {
+    var arrayRules = [{}];
+    for (var i in data) {
+        arrayRules[0][i] = schema.elements;
+    }
+    return arrayRules;
+}
+
+function throwErrors(errors) {
+    if(errors.length > 0) {
+        throw new ModelError(errors.join('\n'));
+    }
+}
---
+++ tests/tests.js
@@ -0,0 +1,527 @@
+var assert = require('assert').strict;
+var {Model, ModelError} = require('../src/model');
+
+describe('Model Builder', function() {
+    describe('Names', function () {
+        it('should reject required but invalid property names', function() {
+            var model = new Model([{
+                name: { required: true }
+            }]);
+            var data = { nrame: 'name' };
+            var error = new ModelError('Required property "name" missing.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Types', function () {
+        it('should reject invalid type for: string', function() {
+            var model = new Model([{
+                name: { type: 'string' }
+            }]);
+            var data = { name: 5 };
+            var error = new ModelError('Type of property "name" should be string.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject invalid type for: number', function() {
+            var model = new Model([{
+                year: { type: 'number' }
+            }]);
+            var data = { year: '2018' };
+            var error = new ModelError('Type of property "year" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject invalid type for: boolean', function() {
+            var model = new Model([{
+                crashed: { type: 'boolean' }
+            }]);
+            var data = { crashed: 1 };
+            var error = new ModelError('Type of property "crashed" should be boolean.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject invalid type for: boolean', function() {
+            var model = new Model([{
+                crashed: { type: 'boolean' }
+            }]);
+            var data = { crashed: 1 };
+            var error = new ModelError('Type of property "crashed" should be boolean.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Enums', function () {
+        it('should reject a value not allowed', function () {
+            var model = new Model([{
+                weather: { enum: ['rainy', 'clear'] }
+            }]);
+            var data = { weather: 'windy' };
+            var error = new ModelError('Property "weather" should be rainy, clear.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should not allow coercion even if type is not enforced', function () {
+            var model = new Model([{
+                number: { enum: ['420', '911'] }
+            }]);
+            var data = { number: 420 };
+            var error = new ModelError('Property "number" should be 420, 911.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Regex', function () {
+        it('should enforce regex matching', function () {
+            var model = new Model([{
+                hash: { match: `[0-9A-F]{4}` }
+            }]);
+            var data = { hash: '9pF2' };
+            var error = new ModelError('Property "hash" should match the following regex [0-9A-F]{4}.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should enforce string type implicitly', function () {
+            var model = new Model([{
+                hash: { match: `[0-9A-F]{4}` }
+            }]);
+            var data = { hash: 3241 };
+            var error = new ModelError('Property "hash" should match the following regex [0-9A-F]{4}.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Range', function () {
+        it('should reject numeric values under range', function () {
+            var model = new Model([{
+                age: { range: [0, 100] }
+            }]);
+            var data = { age: -1 };
+            var error = new ModelError('Property "age" should be a number between 0 and 100.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject numeric values over range', function () {
+            var model = new Model([{
+                age: { range: [0, 100] }
+            }]);
+            var data = { age: 125 };
+            var error = new ModelError('Property "age" should be a number between 0 and 100.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject strings under length', function () {
+            var model = new Model([{
+                nationality: { range: [5, 10] }
+            }]);
+            var data = { nationality: 'us' };
+            var error = new ModelError('Property "nationality" should have a length between 5 and 10.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject strings over length', function () {
+            var model = new Model([{
+                nationality: { range: [5, 10] }
+            }]);
+            var data = { nationality: 'Citizen of the World' };
+            var error = new ModelError('Property "nationality" should have a length between 5 and 10.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Messages', function () {
+        it('should show custom string message for the whole property', function () {
+            var model = new Model([{
+                crashed: {
+                    type: 'boolean',
+                    message: 'mayday'
+                }
+            }]);
+            var data = {crashed: 1};
+            var error = new ModelError('mayday');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should show custom string message for each rule', function () {
+            var model = new Model([{
+                crashed: {
+                    type: 'boolean',
+                    message: {
+                        type: 'mayday'
+                    }
+                }
+            }]);
+            var data = {crashed: 1};
+            var error = new ModelError('mayday');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Functions', function() {
+        it('should display valid nested path', function () {
+            var model = new Model([{
+                location: [{
+                    lon: {
+                        type: 'number',
+                        message: {
+                            type: (path, type) => `"${path}" should be ${type}`
+                        }
+                    }
+                }]
+            }]);
+            var data = { location: {lon: '54.342'} };
+            var error = new ModelError('"location.lon" should be number');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Objects', function() {
+        it('should reject primitive in place of object', function () {
+            var model = new Model([{
+                location: [{
+                    lon: {
+                        type: 'number',
+                    }
+                }]
+            }]);
+            var data = { location: 443 };
+            var error = new ModelError('Type of property "location" should be object.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Primitives', function() {
+        it('should give invalid type for primitive', function () {
+            var model = new Model({
+                type: 'number'
+            });
+            var data = 'hi';
+            var error = new ModelError('Type of property "" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should give invalid type for array of primitives', function () {
+            var model = new Model({
+                type: 'array',
+                elements: {
+                    type: 'number'
+                }
+            });
+            var data = [3, '1', 4];
+            var error = new ModelError('Type of property "1" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Array', function() {
+        it('should reject invalid type inside array', function () {
+            var model = new Model({
+                type: 'array',
+                range: [1, Infinity],
+                elements: {type: 'number'}
+            });
+            var data = [1, 2, 'c'];
+            var error = new ModelError('Type of property "2" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject invalid object property inside array', function () {
+            var model = new Model([{
+                rooms: {
+                    type: 'array',
+                    range: [1, Infinity],
+                    elements: [{
+                        size: {
+                            type: 'number'
+                        },
+                        parke: {
+                            type: 'boolean'
+                        }
+                    }]
+                }
+            }]);
+            var data = {
+                rooms: [{size: 4, parke: true}, {size: 2}, {size: '2'}]
+            };
+            var error = new ModelError('Type of property "rooms.2.size" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should reject invalid type inside nested arrays', function () {
+            var model = new Model({
+                type: 'array',
+                range: [1, 1],
+                elements: {
+                    type: 'array',
+                    elements: {
+                        type: 'array',
+                        elements: { type: 'number' }
+                    }
+                }
+            });
+            var data = [[[2,3],['3',1]]];
+            var error = new ModelError('Type of property "0.1.0" should be number.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+        it('should allow valid case', function () {
+            var model = new Model({
+                type: 'array',
+                elements: [{
+                    size: {
+                        type: 'number'
+                    },
+                    parke: {
+                        type: 'boolean'
+                    }
+                }]
+            });
+            var data = [{size: 4, parke: true}, {size: 2}, {size: 2}];
+            assert.deepStrictEqual(model.validate(data), data);
+        });
+    });
+
+    describe('JavaScript Types', function() {
+        it('should handle JS types', function () {
+            var model = new Model([{
+                name: { type: String, required: true },
+                age: { type: Number, required: true },
+                married: { type: Boolean, required: true },
+                children: {
+                    type: Array,
+                    elements: { type: model }
+                }
+            }]);
+            var data = { name: 'name' };
+            var error = new ModelError('Required property "name" missing.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Model nesting', function () {
+        it('should give errors from nested model', function () {
+            var user = new Model([{
+                name: {
+                    type: 'string',
+                    required: true
+                },
+                email: {
+                    type: 'string',
+                    required: true
+                }
+            }]);
+            var model = new Model([{
+                title: {
+                    type: 'string',
+                    required: true
+                },
+                content: {
+                    type: 'string',
+                    required: true
+                },
+                author: user
+            }]);
+            var data = {
+                title: 'Hola',
+                content: 'Como estas',
+                author: {
+                    name: 'Johnny Bravo',
+                    email: 42
+                }
+            };
+            var error = new ModelError('Type of property "author.email" should be string.');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+    });
+
+    describe('Complex', function () {
+        var model = new Model([{
+            name: {
+                required: true,
+                type: 'string',
+                match: `^[a-zA-Z\x20]*$`,
+                message: {
+                    required: 'name required',
+                    match: 'should be only letters and spaces'
+                }
+            },
+            location: [{
+                lat: {
+                    required: true,
+                    type: 'number',
+                    range: [-90, 90],
+                    message: {
+                        range: 'invalid latitude'
+                    }
+                },
+                lon: {
+                    required: true,
+                    type: 'number',
+                    message: {
+                        required: (path) => `The parameter ${path} is mandatory`,
+                        type: 'Type of longitude is number'
+                    }
+                }
+            }],
+            extra: {
+                required: false,
+                type: 'string',
+                message: (path, type) => `The parameter ${path} should be of type ${type}`
+            },
+            rooms: {
+                type: 'array',
+                elements: [{
+                    type: {
+                        required: true,
+                        type: 'string',
+                        enum: ['double', 'king', 'single'],
+                        message: "Invalid room"
+                    },
+                    price: {
+                        required: false,
+                        type: 'number'
+                    },
+                    currency: {
+                        required: true,
+                        type: 'string',
+                        range: [1, 5]
+                    }
+                }]
+            }
+        }]);
+        var base = {
+            name: 'Bella Vista',
+            location: {
+                lat: 40.334,
+                lon: 12.34235551
+            },
+            extra: '5 Stars',
+            rooms: [
+                {
+                    type: 'double',
+                    price: 58.15,
+                    currency: '$'
+                },
+                {
+                    type: 'single',
+                    price: 13.85,
+                    currency: '$'
+                }
+            ]
+        };
+
+        it('should reject non existing name', function () {
+            var data = clone(base);
+            delete data.name;
+            var error = new ModelError('name required');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+
+        it('should reject invalid name', function () {
+            var data = clone(base);
+            data.name = 'X7!@';
+            var error = new ModelError('should be only letters and spaces');
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+
+        it('should reject invalid name type', function () {
+            var data = {};
+            Object.assign(data, base);
+            data.name = Symbol();
+            var error = new ModelError(
+                'Type of property "name" should be string.\n' +
+                'should be only letters and spaces'
+            );
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+
+        it('should reject invalid name type with default error and regex with custom error', function () {
+            var data = clone(base);
+            data.name = Symbol();
+            var error = new ModelError(
+                'Type of property "name" should be string.\n' +
+                'should be only letters and spaces'
+            );
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+
+        it('should reject invalid lat type', function () {
+            var data = clone(base);
+            data.location.lat = Symbol();
+            var error = new ModelError(
+                'Type of property "location.lat" should be number.\n' +
+                'invalid latitude'
+            );
+            assert.throws(() => {
+                model.validate(data);
+            }, error);
+        });
+
+        it('should allow valid object with non required properties', function () {
+            assert.deepStrictEqual(model.validate(base), base);
+        });
+    });
+});
+
+function clone(obj) {
+    if (obj === null || typeof (obj) !== 'object' || 'isActiveClone' in obj)
+        return obj;
+
+    var temp;
+    if (obj instanceof Date)
+        temp = new obj.constructor(); //or new Date(obj);
+    else
+        temp = obj.constructor();
+
+    for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            obj.isActiveClone = null;
+            temp[key] = clone(obj[key]);
+            delete obj.isActiveClone;
+        }
+    }
+    return temp;
+}
